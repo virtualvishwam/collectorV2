ebbee57292b5b1090869043838bfff67882e8b0e
virtualvishwam <11646200+virtualvishwam@users.noreply.github.com>
2025-05-12 18:32:55 +0000

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MatCardModule, MatButtonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+  title = 'collectorV2';
+}
